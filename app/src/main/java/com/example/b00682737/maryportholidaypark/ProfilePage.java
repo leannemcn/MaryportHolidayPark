f169f6c73b85e8cf3cf4a1c9aa5870c2abfa96eb
[Leanne] <mcnicholl-l@email.ulster.ac.uk>
2018-11-29 21:26:03 +0000

    Menu tab (brings me to different pages) + sign out

--- app/src/main/java/com/example/b00682737/maryportholidaypark/ProfilePage.java
+++ app/src/main/java/com/example/b00682737/maryportholidaypark/ProfilePage.java
@@ -3,6 +3,8 @@ package com.example.b00682737.maryportholidaypark;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,18 +31,46 @@ public class ProfilePage extends AppCompatActivity {
             public void onClick(View view) {
                 firebaseAuth.signOut(); //sign user out
                 finish();
-                startActivity(new Intent (ProfilePage.this, LoginPage.class ));
+                startActivity(new Intent(ProfilePage.this, LoginPage.class));
                 //this will log the user out and return to the login page
             }
         });
 
+    }
 
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent createBooking = new Intent(ProfilePage.this, BookingPage.class);
-                ProfilePage.this.startActivity(createBooking);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-});
-    }
-}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.makeABooking) {
+            Intent myintent = new Intent(ProfilePage.this, BookingPage.class);
+            ProfilePage.this.startActivity(myintent); //to display the booking page once selected
+
+            return false;
+
+
+        } else if (id == R.id.cancelBooking) {
+                Intent cancelintent = new Intent(ProfilePage.this, CancelBooking.class);
+                ProfilePage.this.startActivity(cancelintent); //to display the cancel booking page
+
+                return false;
+            } else if (id == R.id.signOut1) {
+                    Intent signOut = new Intent(ProfilePage.this, LoginPage.class);
+                    ProfilePage.this.startActivity(signOut);
+
+                    return false;
+
+
+                }
+                return super.onOptionsItemSelected(item);
+            }
+
+        }
+
+
