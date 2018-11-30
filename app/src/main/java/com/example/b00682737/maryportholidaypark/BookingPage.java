ece8b62e46abfdf4c179466db86330ee631a27b7
[Leanne] <mcnicholl-l@email.ulster.ac.uk>
2018-11-30 17:07:13 +0000

    Creating a booking & storing this in the database

--- app/src/main/java/com/example/b00682737/maryportholidaypark/BookingPage.java
+++ app/src/main/java/com/example/b00682737/maryportholidaypark/BookingPage.java
@@ -1,28 +1,18 @@
 package com.example.b00682737.maryportholidaypark;
 
 import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.Calendar;
 
 public class BookingPage extends AppCompatActivity {
 
@@ -31,131 +21,111 @@ public class BookingPage extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener mDateSetListener, mDateSetListener2;
     private TextView mDisplayDate2;
 
-    EditText editTextName;
-    Button createBookingg;
+    EditText enterCheckIn, enterCheckOut, editTextName;
+    Button createBooking2;
     Spinner spinnerSpin;
 
     DatabaseReference databaseCaravan;
-
-    //testing below
-   // private FirebaseAuth firebaseAuth3;
-  //  private DatabaseReference databaseReference;
-   // private EditText manyBeds;
-   // private Button createBooking;
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_page);
-        mDisplayDate = (TextView) findViewById(R.id.checkIn);
-        mDisplayDate2 = (TextView) findViewById(R.id.checkOut);
+        //   mDisplayDate = (TextView) findViewById(R.id.checkIn);
+        //mDisplayDate2 = (TextView) findViewById(R.id.checkOut);
 
         databaseCaravan = FirebaseDatabase.getInstance().getReference("bookings");
 
-        editTextName = (EditText) findViewById(R.id.editTextName);
-        createBookingg = (Button) findViewById(R.id.createBookingg);
+        enterCheckIn = (EditText) findViewById(R.id.enterCheckIn);
+        enterCheckOut = (EditText) findViewById(R.id.enterCheckOut);
+        createBooking2 = (Button) findViewById(R.id.createBooking2);
         spinnerSpin = (Spinner) findViewById(R.id.spinnerSpin);
 
-        createBookingg.setOnClickListener(new View.OnClickListener() {
+        createBooking2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addBedroom();
+                addBooking();
 
             }
         });
-      //  firebaseAuth3 = FirebaseAuth.getInstance();
-      // databaseReference = FirebaseDatabase.getInstance().getReference();
-      //  manyBeds = (EditText)findViewById(R.id.manyBeds);
-      //  createBooking = (Button)findViewById(R.id.createBooking);
-
-        mDisplayDate.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-                int month = cal.get(Calendar.MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        BookingPage.this,
-                        android.R.style.Theme_Light, mDateSetListener, year, day, month);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
-
-        mDisplayDate2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Calendar cal2 = Calendar.getInstance();
-                int year = cal2.get(Calendar.YEAR);
-                int day = cal2.get(Calendar.DAY_OF_MONTH);
-                int month = cal2.get(Calendar.MONTH);
-
-                DatePickerDialog dialog2 = new DatePickerDialog(
-                        BookingPage.this,
-                        android.R.style.Theme_Light, mDateSetListener2, year, day, month);
-                dialog2.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog2.show();
-            }
-        });
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month + 1; //this makes january appear as month 1 instead of 0
-                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year );
-                String date = month + "/" + day + "/" + year;
-                mDisplayDate.setText(date);
-            }
-
-        };
-
-        mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month + 1; //this makes january appear as month 1 instead of 0
-                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year );
-                String date = month + "/" + day + "/" + year;
-                mDisplayDate2.setText(date);
-            }
-
-        };
-      //  private void saveUserInformation(){
-          //  String manyBeds = manyBeds.getText().toString().trim();
-
-          //  UserInformation userInformation = new UserInformation(manyBeds);
-           // FirebaseUser user = firebaseAuth3.getCurrentUser();
-           // databaseReference.child(user.getUid()).setValue(userInformation);
-           // Toast.makeText(this, "information saved..", Toast.LENGTH_LONG).show();
-
     }
-   // if(view == createBooking){
-        //    saveUserInformation();
-  //  }
 
-    private void addBedroom(){
-        String name = editTextName.getText().toString().trim();
-        String caravan = spinnerSpin.getSelectedItem().toString();
+        //  mDisplayDate.setOnClickListener(new View.OnClickListener(){
+        //    @Override
+        //   public void onClick(View view){
+        //    Calendar cal = Calendar.getInstance();
+        //   int year = cal.get(Calendar.YEAR);
+        //  int day = cal.get(Calendar.DAY_OF_MONTH);
+        //  int month = cal.get(Calendar.MONTH);
 
-        if(!TextUtils.isEmpty(name)){
-           String id = databaseCaravan.push().getKey(); //to create a unique string inside bookings
+        // DatePickerDialog dialog = new DatePickerDialog(
+        //     BookingPage.this,
+        //    //     android.R.style.Theme_Light, mDateSetListener, year, day, month);
+        // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        // dialog.show();
+        // }
+        //  });
 
-            Caravan caravan1 = new Caravan(id, name, caravan);
-            databaseCaravan.child(id).setValue(caravan1);
+        //  mDisplayDate2.setOnClickListener(new View.OnClickListener(){
+        //   @Override
+        //  public void onClick(View view){
+        //   Calendar cal2 = Calendar.getInstance();
+        //  int year = cal2.get(Calendar.YEAR);
+        //  int day = cal2.get(Calendar.DAY_OF_MONTH);
+        // int month = cal2.get(Calendar.MONTH);
 
-            Toast.makeText(this, "Booking added", Toast.LENGTH_LONG).show();
+        //  DatePickerDialog dialog2 = new DatePickerDialog(
+        //       BookingPage.this,
+        //       android.R.style.Theme_Light, mDateSetListener2, year, day, month);
+        // dialog2.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        // dialog2.show();
+        //  }
+        //  });
+
+        //mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        // @Override
+        // public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        //   month = month + 1; //this makes january appear as month 1 instead of 0
+        //  Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year );
+        // String date = month + "/" + day + "/" + year;
+        // mDisplayDate.setText(date);
+        //    }
+
+        //   };
+
+        //  mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+        //  @Override
+        // public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        //    month = month + 1; //this makes january appear as month 1 instead of 0
+        //    Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year );
+        //   String date = month + "/" + day + "/" + year;
+        //   mDisplayDate2.setText(date);
+        // }
+
+        //  };
+
+        // }
+
+        private void addBooking(){
+            String checkingin = enterCheckIn.getText().toString().trim();
+            String checkingout = enterCheckOut.getText().toString().trim();
+            String caravan = spinnerSpin.getSelectedItem().toString();
 
 
-        }else{
-            Toast.makeText(this, "you should enter a name", Toast.LENGTH_LONG).show();
+            if (!TextUtils.isEmpty(checkingin)) {
+                String id = databaseCaravan.push().getKey(); //to create a unique string inside bookings
+
+                Caravan caravan1 = new Caravan(id, checkingin, caravan,checkingout);
+                databaseCaravan.child(id).setValue(caravan1);
+
+                Toast.makeText(this, "Your booking has been successfully added", Toast.LENGTH_LONG).show();
+
+
+            } else {
+                Toast.makeText(this, "Please fill out required details for making a booking", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
-    }
 
-
-//}
--- app/src/main/java/com/example/b00682737/maryportholidaypark/Caravan.java
+++ app/src/main/java/com/example/b00682737/maryportholidaypark/Caravan.java
@@ -3,28 +3,35 @@ package com.example.b00682737.maryportholidaypark;
 public class Caravan {
 
     String caravanId;
-    String caravanName;
-    String caravanBeds;
+    String caravanCheckIn;
+    String caravanBedrooms;
+    String caravanCheckOut;
+
 
     public Caravan(){
 
     }
 
-    public Caravan(String caravanId, String caravanName, String caravanBeds) {
+    public Caravan(String caravanId, String caravanCheckIn, String caravanBedrooms, String caravanCheckOut) {
         this.caravanId = caravanId;
-        this.caravanName = caravanName;
-        this.caravanBeds = caravanBeds;
+        this.caravanCheckIn = caravanCheckIn;
+        this.caravanBedrooms = caravanBedrooms;
+        this.caravanCheckOut = caravanCheckOut;
     }
 
     public String getCaravanId() {
         return caravanId;
     }
 
-    public String getCaravanName() {
-        return caravanName;
+    public String getCaravanCheckIn() {
+        return caravanCheckIn;
     }
 
-    public String getCaravanBeds() {
-        return caravanBeds;
+    public String getCaravanBedrooms() {
+        return caravanBedrooms;
+    }
+
+    public String getCaravanCheckOut() {
+        return caravanCheckOut;
     }
 }
