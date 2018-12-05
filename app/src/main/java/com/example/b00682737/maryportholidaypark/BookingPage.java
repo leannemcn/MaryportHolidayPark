46905f2bc351004e6ab7495988dd8ba2f6b7655d
[Leanne] <mcnicholl-l@email.ulster.ac.uk>
2018-12-05 12:05:55 +0000

    Fixing image in profile page

--- app/src/main/java/com/example/b00682737/maryportholidaypark/BookingPage.java
+++ app/src/main/java/com/example/b00682737/maryportholidaypark/BookingPage.java
@@ -1,16 +1,16 @@
 package com.example.b00682737.maryportholidaypark;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.text.TextUtils;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Spinner;
+        import android.widget.Toast;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
 
 public class BookingPage extends AppCompatActivity {
 
@@ -45,33 +45,33 @@ public class BookingPage extends AppCompatActivity {
     }
 
 
-        private void addBooking(){
-            String checkingin = enterCheckIn.getText().toString().trim();
-            String checkingout = enterCheckOut.getText().toString().trim();
-            String caravan = spinnerSpin.getSelectedItem().toString();
-            String extras = spinnerSpin2.getSelectedItem().toString();
-            //will store the following data to the database
+    private void addBooking(){
+        String checkingin = enterCheckIn.getText().toString().trim();
+        String checkingout = enterCheckOut.getText().toString().trim();
+        String caravan = spinnerSpin.getSelectedItem().toString();
+        String extras = spinnerSpin2.getSelectedItem().toString();
+        //will store the following data to the database
 
 
-            if (!TextUtils.isEmpty(checkingin)) {
-                String id = databaseCaravan.push().getKey(); //to create a unique string inside bookings
+        if (!TextUtils.isEmpty(checkingin)) {
+            String id = databaseCaravan.push().getKey(); //to create a unique string inside bookings
 
-               Caravan caravan1 = new Caravan(id, checkingin, extras, caravan, checkingout);
-               databaseCaravan.child(id).setValue(caravan1);
+            Caravan caravan1 = new Caravan(id, checkingin, extras, caravan, checkingout);
+            databaseCaravan.child(id).setValue(caravan1);
 
-               Toast.makeText(this, "Congratulations! Your booking has been successfully added", Toast.LENGTH_LONG).show();
-               finish();
-               startActivity(new Intent(BookingPage.this, ProfilePage.class));
-               //will bring user back to their profile page if booking is successful
+            Toast.makeText(this, "Congratulations! Your booking has been successfully added", Toast.LENGTH_LONG).show();
+            finish();
+            startActivity(new Intent(BookingPage.this, ProfilePage.class));
+            //will bring user back to their profile page if booking is successful
 
 
-            } else {
-               Toast.makeText(this, "Error! Please fill out required details for making a booking", Toast.LENGTH_LONG).show();
-               //will keep user on booking page if booking has been unsuccessful
+        } else {
+            Toast.makeText(this, "Error! Please fill out required details for making a booking", Toast.LENGTH_LONG).show();
+            //will keep user on booking page if booking has been unsuccessful
 
-                }
-            }
         }
+    }
+}
 
 
 
